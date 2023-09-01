6874fb397d658ff05bdd83019cda91db68b33755
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-09-01 10:40:19 +0530

    learning django

--- myapp/website/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    name=models.CharField(max_length=200)
-    college=models.CharField(max_length=300)
-    age=models.IntegerField(max_length=10)
-    is_active=models.BooleanField(default=False)
